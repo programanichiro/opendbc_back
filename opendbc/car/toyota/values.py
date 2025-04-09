41f53d0f4c73c956b50d009284f558ba11535e06
ichiroMax <kawaoka@yuye.co.jp>
2025-04-09 19:15:28 +0900

    公式縦制御からj_egoのスムーズ処理を消す

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -36,11 +36,7 @@ class CarControllerParams:
 
   def __init__(self, CP):
     if CP.flags & ToyotaFlags.RAISED_ACCEL_LIMIT:
-      if CP.carFingerprint in TSS2_CAR:
-        self.ACCEL_MAX = 2.0
-      else:
-        #TSSP
-        self.ACCEL_MAX = 1.5
+      self.ACCEL_MAX = 2.0
     else:
       self.ACCEL_MAX = 2.0  #1.5では発進が鈍る？ # m/s2, lower than allowed 2.0 m/s^2 for tuning reasons
     self.ACCEL_MIN = -3.5  # m/s2
