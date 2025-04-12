48061f7a464d19db60c581f586518bf8c1db8c90
ichiroMax <kawaoka@yuye.co.jp>
2025-04-12 16:53:19 +0900

    これで動くか検証。interpも数字ならfloatへキャスト。

--- opendbc/car/common/numpy_fast.py
+++ opendbc/car/common/numpy_fast.py
@@ -1,37 +1,19 @@
-import os
 import numpy as np
 from numbers import Number
 
 def clip(x, lo, hi):
-    # return a
-    # a = max(lo, min(hi, x))
     np_ = np.clip(x, lo, hi)
     return float(np_)
 
+
 def interp(x, xp, fp):
   np_ = np.interp(x, xp, fp)
-  N = len(xp)
-
-  def get_interp(xv):
-    hi = 0
-    while hi < N and xv > xp[hi]:
-      hi += 1
-    low = hi - 1
-    return fp[-1] if hi == N and xv > xp[low] else (
-      fp[0] if hi == 0 else
-      (xv - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]) + fp[low])
-
-  a = [get_interp(v) for v in x] if hasattr(x, '__iter__') else get_interp(x)
-  return a
-  if np_ == None or not isinstance(np_, Number) or a != np_:
-    with open('/tmp/debug_out_v','w') as fp:
-      fp.write("interp:%f" % (a))
-    return a
+  if isinstance(np_, Number):
+    return float(np_)
   else:
     return np_
 
 
 def mean(x):
   return np.mean(x)
-  return sum(x) / len(x)
 
