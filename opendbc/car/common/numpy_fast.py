03663a439237cca83d8e342cd9409bc8ad096174
ichiroMax <kawaoka@yuye.co.jp>
2025-04-12 11:13:33 +0900

    ダメだエラー。Noneを疑う

--- opendbc/car/common/numpy_fast.py
+++ opendbc/car/common/numpy_fast.py
@@ -4,7 +4,7 @@ from numbers import Number
 def clip(x, lo, hi):
   np_ = np.clip(x, lo, hi)
   a = max(lo, min(hi, x))
-  if not isinstance(np_, Number) or a != np_:
+  if np_ == None or not isinstance(np_, Number) or a != np_:
     with open('/tmp/debug_out_o','w') as fp:
       fp.write("clip:%f, %f,%f,%f" % (a , x,lo,hi))
     return a
@@ -26,7 +26,7 @@ def interp(x, xp, fp):
       (xv - xp[low]) * (fp[hi] - fp[low]) / (xp[hi] - xp[low]) + fp[low])
 
   a = [get_interp(v) for v in x] if hasattr(x, '__iter__') else get_interp(x)
-  if not isinstance(np_, Number) or a != np_:
+  if np_ == None or not isinstance(np_, Number) or a != np_:
     with open('/tmp/debug_out_v','w') as fp:
       fp.write("interp:%f" % (a))
     return a
