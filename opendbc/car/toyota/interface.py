08caf1858f23a52f17f17747a761f18f1bdea6c3
ichiroMax <kawaoka@yuye.co.jp>
2025-06-12 19:07:03 +0900

    ForceHybridVehicleに対応。

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -68,7 +68,7 @@ class CarInterface(CarInterfaceBase):
     ret.enableDsu = len(found_ecus) > 0 and Ecu.dsu not in found_ecus and candidate not in (NO_DSU_CAR | UNSUPPORTED_DSU_CAR) \
                                         and not (ret.flags & ToyotaFlags.SMART_DSU) and not (ret.flags & ToyotaFlags.DSU_BYPASS.value)
 
-    if Ecu.hybrid in found_ecus:
+    if (Ecu.hybrid in found_ecus) or Params().get_bool("ForceHybridVehicle") == True:
       ret.flags |= ToyotaFlags.HYBRID.value
 
     if Params().get_bool("AccelMethodSwitch") == True: # ichiropilot
