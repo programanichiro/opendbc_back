cd96d7e5d4e96ef838fbdde87293a8de8ef1e3a1
ichiroMax <kawaoka@yuye.co.jp>
2025-06-23 10:36:55 +0900

    create_acc_set_command,これ可能か？

--- opendbc/car/toyota/toyotacan.py
+++ opendbc/car/toyota/toyotacan.py
@@ -179,3 +179,9 @@ def create_ui_command(packer, steer, chime, left_line, right_line, left_lane_dep
     ]})
 
   return packer.make_can_msg("LKAS_HUD", 0, values)
+
+def create_acc_set_command(packer):
+  values = {
+    "CRUISE_ACTIVE": 1, #これでSETが点く？
+  }
+  return packer.make_can_msg("PCM_CRUISE", 0, values)
