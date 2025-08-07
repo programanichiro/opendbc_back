96d45fc7a8966f3d0493163759a0461c437a0936
ichiroMax <kawaoka@yuye.co.jp>
2025-08-07 09:21:03 +0900

    だめだな。parser.updateの中で呼ばれているpandaが動いているのか？保留。

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -64,32 +64,7 @@ class CarInterface(CarInterfaceBase):
     # Detect 0x343 on bus 2, if detected on bus 2 and is not TSS 2, it means DSU is bypassed
     if not (ret.flags & ToyotaFlags.SMART_DSU) and 0x343 in fingerprint[2] and candidate not in TSS2_CAR:
       #SMART_DSUと共存できない。
-      cam_messages = [
-        ("ACC_CONTROL", 33),
-        ]
-      packer = CANPacker(DBC[candidate][Bus.pt])
-      parser = CANParser(DBC[candidate][Bus.pt], cam_messages, 2)
-      can_valid = False
-      for i in range(1, 100):
-        t = int(0.01 * i * 1e9)
-        values = {
-          "ACCEL_CMD": 0,
-          "ACC_TYPE": 1,
-          "DISTANCE": 0,
-          "MINI_CAR": 0,
-          "PERMIT_BRAKING": 0,
-          "RELEASE_STANDSTILL": 0,
-          "CANCEL_REQ": 0,
-          "ALLOW_LONG_PRESS": 1,
-          "ACC_CUT_IN": 0,
-        }
-        msg = packer.make_can_msg("ACC_CONTROL", 0, values)
-        parser.update([t, [msg]])
-        if parser.can_valid:
-          can_valid = True
-          break
-      if can_valid:
-      #if Params().get_bool("IgnoreRerouteHarness") == False: #リルートハーネス装着の区別ができないので、機能にスイッチをつけた。
+      if Params().get_bool("IgnoreRerouteHarness") == False: #リルートハーネス装着の区別ができないので、機能にスイッチをつけた。
         ret.flags |= ToyotaFlags.DSU_BYPASS.value
       else:
         #DSUが接続されているTSSP車両
