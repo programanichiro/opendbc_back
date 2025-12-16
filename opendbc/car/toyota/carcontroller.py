6bdd51bca331eeb2eda0256284a7bbb7f016d452
ichiroMax <kawaoka@yuye.co.jp>
2025-12-17 08:39:00 +0900

    enableDsu, openpilotLongitudinalControlの状態確認。

--- opendbc/car/toyota/carcontroller.py
+++ opendbc/car/toyota/carcontroller.py
@@ -516,4 +516,8 @@ class CarController(CarControllerBase):
     new_actuators.accel = self.accel
 
     self.frame += 1
+
+    with open('/tmp/debug_out_v','w') as fp:
+      fp.write("enableDsu:%d, openpilotLongitudinalControl:%d" % (int(self.CP.enableDsu), int(self.CP.openpilotLongitudinalControl)))
+
     return new_actuators, can_sends
